ec088f3f9eef8ef38eb1e52c09c3d209ac649cf0
SergiuOvidiu472 <aa21.soraciulete72@iespabloserrano.com>
2025-09-25 08:53:43 +0200

    Añadir: Función para preguntar el nombre al usuario y función para saludar

--- Hola.java
+++ Hola.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class Hola {
-
+    public static String preguntarNombre ()
+    {
+        System.out.print ("Como te llamas?: ");
+        String nombre = new Scanner (System.in).nextLine ();
+        return nombre;
+    }
+    public static String saludo (String nombre)
+    {
+        return "Hola, " + nombre;
+    }
     public static void main(String[] args) {
-        
-        System.out.println("Hola");
+        String nombre = preguntarNombre ();
+        String saludo = saludo (nombre);
+        System.out.println(saludo);
     }
 }
